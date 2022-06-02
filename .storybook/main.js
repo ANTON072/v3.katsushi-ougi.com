cf08687a989db7c28bee8b9b1789620595070ff4
Katsushi OUGI <ougi@to-r.net>
2022-06-02 14:39:36 +0900

    storybookの設定を修正

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 module.exports = {
   stories: [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../domain/**/*.stories.mdx",
+    "../domain/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-links",

5ae8612042bfa8bde789ecf863a3f5517ad507ed
Katsushi OUGI <ougi@to-r.net>
2022-06-07 12:43:43 +0900

    wp-rest apiにアクセスするクラスの追加

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    WORDPRESS_URL: process.env.WORDPRESS_URL,
+  },
 };
 
 module.exports = nextConfig;

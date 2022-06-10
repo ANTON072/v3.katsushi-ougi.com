72b5e8360fab249b7ad38ef5a82ee99deaf4d358
Katsushi OUGI <ougi@to-r.net>
2022-06-10 20:25:39 +0900

    Revert "sidebar remove"
    This reverts commit e7fe03e3122ed8896bb9ae1b8608be3760883177.

---
+++ components/Sidebar/index.tsx
@@ -0,0 +1,25 @@
+import { useMemo } from "react";
+import About from "./About";
+import Topics from "./Topics";
+import useTags from "../../libs/hooks/useTags";
+
+const Sidebar = () => {
+  const { tags } = useTags();
+
+  const tagItems = useMemo(() => {
+    return tags.map((tag) => ({
+      label: tag.name,
+      href: `/tags/${tag.slug}`,
+      count: tag.count,
+    }));
+  }, [tags]);
+
+  return (
+    <aside className="pt-[var(--padding)] border-t-[1px] border-t-solid border-t-[color:var(--grey3)] md:w-[var(--sidebar-width)] md:pt-0 md:pl-[var(--padding)] md:border-t-0">
+      <About />
+      <Topics items={tagItems} />
+    </aside>
+  );
+};
+
+export default Sidebar;

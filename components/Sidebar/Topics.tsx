64bf904fa076243c688671f880f083ff7fa84a6f
Katsushi OUGI <ougi@to-r.net>
2022-06-10 20:27:53 +0900

    add sidebar

---
+++ components/Sidebar/Topics.tsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import { FC } from "react";
+
+type Item = {
+  label: string;
+  href: string;
+  count: number;
+};
+
+export type TopicsProps = {
+  items: Item[];
+};
+
+const Topics: FC<TopicsProps> = ({ items }) => {
+  return (
+    <div>
+      <h2 className="mb-[calc(var(--padding)/4)] font-[700] font-en text-[24px]">
+        TOPICS
+      </h2>
+      <ul className="pl-[1em] list-disc text-link">
+        {items.map(({ label, href, count }) => (
+          <li key={label} className="leading-[1.7]">
+            <>
+              <Link href={href}>
+                <a>{label}</a>
+              </Link>
+              <span className="pl-[5px]">({count})</span>
+            </>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Topics;

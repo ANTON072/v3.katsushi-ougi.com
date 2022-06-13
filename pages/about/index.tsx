be477e6d466f8db69f827fc72aa073ac8a79beb1
Katsushi OUGI <ougi@to-r.net>
2022-06-13 15:49:03 +0900

    aboutページの作成

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,12 +1,31 @@
-import { NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
+import { PostBody } from "../../components/article";
 import Heading from "../../components/Heading";
+import fetch from "../../libs/polyfill/fetch";
+import { WPPost } from "../../libs/wpapi/interfaces";
+import { WPAPIURLFactory } from "../../libs/wpapi/UrlBuilder";
 
-const AboutPage: NextPage = () => {
+const urlBuilder = WPAPIURLFactory.init(process.env.WORDPRESS_URL)
+  .postType("pages")
+  .slug("about");
+
+const AboutPage: NextPage<{ page: WPPost }> = ({ page }) => {
   return (
     <>
       <Heading title="about" />
+      <PostBody body={page.content.rendered} />
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+  const page = await fetch(urlBuilder.getURL());
+
+  return {
+    props: {
+      page: page[0],
+    },
+  };
 };
 
 export default AboutPage;

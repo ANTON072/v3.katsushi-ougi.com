195640ee525913a889842b08cc81414619a19873
Katsushi OUGI <ougi@to-r.net>
2022-06-13 16:05:48 +0900

    favicon差し替え

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import Layout from "../components/Layout";
 import "../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );

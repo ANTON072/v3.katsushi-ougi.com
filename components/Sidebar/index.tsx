d670221e53e070539fa99bd46aa94172777d59d2
Katsushi OUGI <ougi@to-r.net>
2022-06-13 16:56:19 +0900

    サイドバー固定

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -2,9 +2,33 @@ import { useMemo } from "react";
 import About from "./About";
 import Topics from "./Topics";
 import useTags from "../../libs/hooks/useTags";
+import { useMount } from "react-use";
+import FixedSidebar from "../../libs/FixedSidebar";
+
+const initialize = () => {
+  if (window !== undefined) {
+    const fixedSidebar = new FixedSidebar();
+
+    if (window.matchMedia("(min-width: 992px)").matches) {
+      /** PC用ブレークポイント */
+      fixedSidebar.scrollTrigger.enable();
+    } else {
+      /** SP用ブレークポイント */
+      fixedSidebar.scrollTrigger.disable();
+    }
+  }
+};
 
 const Sidebar = () => {
   const { tags } = useTags();
+
+  useMount(() => {
+    if (window !== undefined) {
+      const mediaQueryList = window.matchMedia("(max-width:767px)");
+      mediaQueryList.addEventListener("change", initialize);
+      mediaQueryList.dispatchEvent(new Event("change"));
+    }
+  });
 
   const tagItems = useMemo(() => {
     return tags.map((tag) => ({
@@ -16,8 +40,10 @@ const Sidebar = () => {
 
   return (
     <aside className="pt-[var(--padding)] border-t-[1px] border-t-solid border-t-[color:var(--grey3)] md:w-[var(--sidebar-width)] md:pt-0 md:pl-[var(--padding)] md:border-t-0">
-      <About />
-      <Topics items={tagItems} />
+      <div id="sidebar_contents">
+        <About />
+        <Topics items={tagItems} />
+      </div>
     </aside>
   );
 };

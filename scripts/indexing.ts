c6988e77533383f6f7ba047a54a9a91a6fa6044a
Katsushi OUGI <ougi@to-r.net>
2022-06-14 16:45:56 +0900

    アルゴリアにインデックスを登録

---
+++ scripts/indexing.ts
@@ -0,0 +1,41 @@
+import algoliasearch from "algoliasearch";
+import { WPAPIURLFactory } from "../libs/wpapi/UrlBuilder";
+import { listAllPosts } from "../libs/wpUtils";
+
+const urlBuilder = WPAPIURLFactory.init(process.env.WORDPRESS_URL as string)
+  .postType("posts")
+  .startAt(1);
+
+(async () => {
+  try {
+    const ALGOLIA_APP_ID = process.env.ALGOLIA_APP_ID as string;
+    const ALGOLIA_API_KEY = process.env.ALGOLIA_API_KEY as string;
+    const ALGOLIA_INDEX_NAME = process.env.ALGOLIA_INDEX_NAME as string;
+
+    const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_API_KEY);
+
+    const index = client.initIndex(ALGOLIA_INDEX_NAME);
+
+    const posts = await listAllPosts(urlBuilder);
+
+    const formattedPosts = posts.map((post) => ({
+      objectID: post.id,
+      slug: post.slug,
+      title: post.title.rendered,
+      content: post.content.rendered,
+      tags: post.tags.map((tag) => tag.name),
+    }));
+
+    index
+      .saveObjects(formattedPosts)
+      .wait()
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  } catch (error) {
+    console.error(error);
+  }
+})();

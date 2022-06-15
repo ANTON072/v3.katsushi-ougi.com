0418d50e6437345ea789ee27b7130e99d70a48a3
Katsushi OUGI <ougi@to-r.net>
2022-06-15 17:35:36 +0900

    不要なCSSの読み込みを削除

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,12 +8,6 @@ function MyApp({ Component, pageProps }: AppProps) {
     <Layout>
       <Head>
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/instantsearch.css@7.4.5/themes/satellite-min.css"
-          integrity="sha256-TehzF/2QvNKhGQrrNpoOb2Ck4iGZ1J/DI4pkd2oUsBc="
-          crossOrigin="anonymous"
-        ></link>
       </Head>
       <Component {...pageProps} />
     </Layout>
